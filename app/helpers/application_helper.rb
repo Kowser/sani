6837daa03a9df319dcbc74b46ea477889d9a3a9f
Michal Kaszubowski <MrKowser@gmail.com>
2016-01-27 13:37:45 -0800

    wip

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+	def link_to_add_unit(f)
+	  fields = f.fields_for(:units, f.object.units.build, child_index: "UNIT-#{99999}") do |u|
+	    render('units', u: u)
+	  end
+	  link_to('Add Room', '#', id: 'add-unit', data: { id: "UNIT-#{99999}", fields: fields.gsub('\n', '') })
+	end
 end
--- app/models/facility.rb
+++ app/models/facility.rb
@@ -1,4 +1,5 @@
 class Facility < ActiveRecord::Base
 	validates_presence_of :name, :address, :city, :state, :zip, :phone
 	has_many :units
+	accepts_nested_attributes_for :units
 end
