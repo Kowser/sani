51bde0156b0d6e58d0c779c5415010d2c977dec6
Michal Kaszubowski <MrKowser@gmail.com>
2016-02-18 17:09:26 -0800

    fixes add/remove staff missing current facility errors

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,12 @@ Rails.application.routes.draw do
     get 'logout', to: 'devise/sessions#destroy'
     get 'login', to: 'devise/sessions#new'
   end
-  resources :facilities_users, only: [:create, :destroy]
 
   resources :facilities do
   # FACILITIES NESTED PATHS
-    controller 'dashboard' do
-      get 'dashboard', action: 'index'
-    end
-
-    resources :staff
+    # controller 'dashboard' do
+    #   get 'dashboard', action: 'index'
+    # end
     controller 'staff' do
       post 'create_staff', action: 'create' #resolves singular/plural staff routing conflict; standard path defaults to 'show'
       post 'find_staff', action: 'find_staff'
@@ -21,13 +18,17 @@ Rails.application.routes.draw do
       post 'remove_staff/:id', action: 'remove_staff'
     end
 
-    resources :invoices
-      post 'receive_payments', to: 'invoices#receive_payments'
-    resources :payments
-      post 'deposit_payments',to: 'payments#deposit_payments'
-    resources :maintenance_requests
+    get 'dashboard', to: 'dashboard#index'
+    post 'receive_payments', to: 'invoices#receive_payments'
+    post 'deposit_payments',to: 'payments#deposit_payments'
 
+
+    resources :invoices
+    resources :payments
+    resources :staff
+    resources :maintenance_requests
     resources :residents
+    resources :facilities_users, only: [:create, :destroy]
   end
   # END FACILITIES NESTED PATH
   
