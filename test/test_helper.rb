52fdaaa14a5afe72c6aca7f54ff2e332a2c5f8c4
Michal Kaszubowski <MrKowser@gmail.com>
2016-03-17 04:23:39 -0700

    wip

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -16,7 +16,7 @@ class ActiveSupport::TestCase
   def log_in_as(user, options = {})
     password    = options[:password]    || 'password'
     remember_me = options[:remember_me] || '1'
-    if integration_test? = defined?(post_via_redirect)
+    if integration_test = defined?(post_via_redirect)
       post login_path, session: { email: user.email, password: password, remember_me: remember_me }
     else
       session[:user_id] = user.id
